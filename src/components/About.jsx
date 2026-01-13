0a221f0995d393d59a0d25b699b13b723ef69efc
Abdul Noor <116881958+Ismailn16@users.noreply.github.com>
2026-01-13 00:12:48 +0000

    comma

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                     <h3 className='text-3xl font-semibold mb-8'><span className='primary-color'>About Me!</span></h3>
                     <div className='space-y-6 text-base md:text-lg leading-relaxed'>
                         <p className='text-white/90'>
-                            Hi, I'm Abdul Noor also known as Ismail! I'm a Software Engineering graduate from UWE Bristol with a passion for Financial Technology and Web3. My academic journey culminated in a dissertation exploring the intersection of blockchain technology and modern online banking, which sparked my fascination with innovative finance solutions.
+                            Hi, I'm Abdul Noor, also known as Ismail! I'm a Software Engineering graduate from UWE Bristol with a passion for Financial Technology and Web3. My academic journey culminated in a dissertation exploring the intersection of blockchain technology and modern online banking, which sparked my fascination with innovative finance solutions.
                         </p>
                         <p className='text-white/90'>
                             Currently working in telecommunications sales, I love all things tech and am particularly drawn to the intersection of finance and blockchain technology. My interest extends beyond traditional banking applications, exploring how concepts like smart contracts and decentralised storage systems such as IPFS can be applied across different areas of finance.
